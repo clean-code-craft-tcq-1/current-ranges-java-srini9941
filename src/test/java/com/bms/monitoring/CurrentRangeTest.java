46f1e9ade8e8fce7f7e740dcd40916bbe1e3543e
srini9941 <srinidhi994141@gmail.com>
2021-08-06 00:45:33 +0530

    Implement failing test

--- src/test/java/com/bms/monitoring/CurrentRangeTest.java
+++ src/test/java/com/bms/monitoring/CurrentRangeTest.java
@@ -3,27 +3,48 @@ package com.bms.monitoring;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CurrentRangeTest {
+public class CurrentRangeTest {
 
     @Test
-    void testForNullInput() {
+    public void testForNullInput() {
         Map<String, Integer> result = CurrentRange.checkRangeReadings(null);
 
         assertNull(result);
     }
 
     @Test
-    void testForEmptyList() {
+    public void testForEmptyList() {
         List<Integer> numberList = new ArrayList<>();
 
         Map<String, Integer> result = CurrentRange.checkRangeReadings(numberList);
 
         assertNull(result);
+    }
+
+    @Test
+    public void testForList() {
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(3);
+        numberList.add(3);
+        numberList.add(5);
+        numberList.add(4);
+        numberList.add(10);
+        numberList.add(11);
+        numberList.add(12);
+
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("3-5", 4);
+        expected.put("10-12", 3);
+
+        Map<String, Integer> result = CurrentRange.checkRangeReadings(numberList);
+
+        assertEquals(expected, result);
     }
 
 }
